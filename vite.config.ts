650c7003c740edad10ac738156d9f3eb3bd5dc05
Merve Metinoglu <mervemetinogluu@gmail.com>
2023-01-23 20:33:45 +0300

    fix: vite config alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
 		alias: [
 			{
 				find: '@',
-				replacement: '/src',
+				replacement: path.resolve(__dirname, './src'),
 			},
 		],
 	},
